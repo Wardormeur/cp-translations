29b80cf3c1c815c0554ab70083adb6717d0bb552
Daniel Brierton <daniel@brierton.me>
2018-01-11 14:54:37 +0000

    Updated tests to include new languages

---
+++ test/supportedLocales.js
@@ -0,0 +1,8 @@
+module.exports = [
+  'bg_BG', 'de_DE', 'el_GR', 'en_US', 'es_AR',
+  'es_ES', 'et_EE', 'fr_FR', 'hi_IN', 'it_IT',
+  'ja_JP', 'lt_LT', 'mt_MT', 'nl_NL', 'pl_PL',
+  'pt_PT', 'ro_RO', 'sk_SK', 'sl_SI', 'tr_TR',
+  'uk_UA', 'zh_CN', 'id_ID', 'si_LK', 'ur_PK',
+  'zh_TW', 'fil_PH',
+];
