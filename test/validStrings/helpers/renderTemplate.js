fb58dd752364a8f66c8494bb3625d4cec3935d61
Wardormeur <wardormeur@gmail.com>
2019-03-22 13:58:48 +0000

    Exclude ENOENT for new template emails

--- test/validStrings/helpers/renderTemplate.js
+++ test/validStrings/helpers/renderTemplate.js
@@ -6,7 +6,8 @@ module.exports = (templateName, locale, data) => {
     const templateDir = path.join(__dirname, '../../../', 'email-templates', `${templateName}-${locale}`);
     const template = new EmailTemplate(templateDir);
     template.render(data, (err, result) => {
-      if (err) return reject(err);
+      if (err && err.code !== 'ENOENT') return reject(err);
+      if (err.code === 'ENOENT') console.warn('File not found', templateDir);
       resolve(result);
     });
   });
